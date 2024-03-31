a5c1486c27947921985a1738ff3d785e7a1d6843
RAMAN <raman.programmer87@gmail.com>
2024-04-01 02:03:14 +0330

    add room again

--- RollMovie/app/build.gradle.kts
+++ RollMovie/app/build.gradle.kts
@@ -98,10 +98,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Room
-//    implementation("androidx.room:room-runtime:2.6.1")
-//    implementation("androidx.room:room-ktx:2.6.1")
-//    annotationProcessor("androidx.room:room-compiler:2.6.1")
-//    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 
     // kotlin flow ->
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
