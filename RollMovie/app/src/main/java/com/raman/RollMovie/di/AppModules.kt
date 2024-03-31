e78b59611281d602a2d644c0939699871a3bdc10
RAMAN <raman.programmer87@gmail.com>
2024-04-01 02:00:42 +0330

    add MovieRemoteDataSource instants to di modules

--- RollMovie/app/src/main/java/com/raman/RollMovie/di/AppModules.kt
+++ RollMovie/app/src/main/java/com/raman/RollMovie/di/AppModules.kt
@@ -2,8 +2,13 @@ package com.raman.RollMovie.di
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.room.Room
 import com.google.firebase.auth.FirebaseAuth
 import com.raman.RollMovie.model.api.ApiService
+import com.raman.RollMovie.model.db.FavoriteDao
+import com.raman.RollMovie.model.db.MovieDao
+import com.raman.RollMovie.model.db.RollMovieDatabase
+import com.raman.RollMovie.model.repo.movie.MovieRemoteDataSource
 import com.raman.RollMovie.utils.createApiService
 import dagger.Module
 import dagger.Provides
@@ -32,6 +37,34 @@ object AppModules {
     @Singleton
     fun provideFirebase(): FirebaseAuth {
         return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieDao( db :RollMovieDatabase ): MovieDao {
+        return db.movieDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavoriteDao( db :RollMovieDatabase ): FavoriteDao {
+        return db.favoriteDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteMovieDataSource( apiService: ApiService ): MovieRemoteDataSource{
+        return MovieRemoteDataSource(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase( @ApplicationContext context: Context ) :RollMovieDatabase {
+        return Room.databaseBuilder(
+            context,
+            RollMovieDatabase::class.java,
+            "app_database.db"
+        ).build()
     }
 
 }
