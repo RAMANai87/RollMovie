7bba71c69303b4b8a970daa17346106813924258
RAMAN <raman.programmer87@gmail.com>
2024-05-18 14:46:09 +0330

    complete mappers and DetailMapper.kt & FavoriteMapper.kt

---
+++ RollMovie/app/src/main/java/com/raman/RollMovie/utils/mapper/DetailMapper.kt
@@ -0,0 +1,33 @@
+package com.raman.RollMovie.utils.mapper
+
+import com.raman.RollMovie.model.data.detail.DetailModel
+import com.raman.RollMovie.model.data.detail.movie.DetailResponse
+import com.raman.RollMovie.model.data.detail.tv.TvShowDetail
+
+fun detailMapperMovie(data :DetailResponse) :DetailModel{
+    return DetailModel(
+        data.poster_path,
+        data.original_title,
+        data.vote_average,
+        data.runtime,
+        data.spoken_languages,
+        data.genres,
+        data.overview,
+        data.release_date,
+        data.production_companies
+    )
+}
+
+fun detailMapperTvShow(data :TvShowDetail) :DetailModel{
+    return DetailModel(
+        data.poster_path,
+        data.original_name,
+        data.vote_average,
+        data.episode_run_time.lastIndex,
+        data.spoken_languages,
+        data.genres,
+        data.overview,
+        data.last_air_date,
+        data.production_companies
+    )
+}
