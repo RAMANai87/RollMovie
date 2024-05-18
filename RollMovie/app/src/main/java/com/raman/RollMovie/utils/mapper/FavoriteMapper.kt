429c32ef19608042d24166d27bd3aa0089cd594a
RAMAN <raman.programmer87@gmail.com>
2024-05-18 14:55:54 +0330

    add a function to map the model and set into database

--- RollMovie/app/src/main/java/com/raman/RollMovie/utils/mapper/FavoriteMapper.kt
+++ RollMovie/app/src/main/java/com/raman/RollMovie/utils/mapper/FavoriteMapper.kt
@@ -2,8 +2,9 @@ package com.raman.RollMovie.utils.mapper
 
 import com.raman.RollMovie.model.data.FavoriteModel
 import com.raman.RollMovie.model.data.detail.movie.DetailResponse
+import com.raman.RollMovie.model.data.detail.tv.TvShowDetail
 
-fun favoriteMapper(data :DetailResponse) :FavoriteModel{
+fun favoriteMapperMovie(data :DetailResponse) :FavoriteModel{
     return FavoriteModel(
         data.id,
         data.original_title,
@@ -11,6 +12,20 @@ fun favoriteMapper(data :DetailResponse) :FavoriteModel{
         data.poster_path,
         data.vote_average,
         data.release_date,
-        data.adult
+        data.adult,
+        true
+    )
+}
+
+fun favoriteMapperTvShow(data :TvShowDetail) :FavoriteModel{
+    return FavoriteModel(
+        data.id,
+        data.original_name,
+        data.overview,
+        data.poster_path,
+        data.vote_average,
+        data.last_air_date,
+        data.adult,
+        false
     )
 }
