30a78281ccb316824cf169503ada00e7cbb7fd9a
RAMAN <raman.programmer87@gmail.com>
2024-05-18 19:48:33 +0330

    refresh the database version to 3

--- RollMovie/app/src/main/java/com/raman/RollMovie/model/db/RollMovieDatabase.kt
+++ RollMovie/app/src/main/java/com/raman/RollMovie/model/db/RollMovieDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.raman.RollMovie.model.data.FavoriteModel
 import com.raman.RollMovie.model.data.MovieModel
 
-@Database(entities = [MovieModel::class, FavoriteModel::class], version = 2, exportSchema = false)
+@Database(entities = [MovieModel::class, FavoriteModel::class], version = 3, exportSchema = false)
 abstract class RollMovieDatabase :RoomDatabase(){
     abstract fun movieDao() :MovieDao
     abstract fun favoriteDao() :FavoriteDao
