1d2a3cc37efb12b39ad554bc6c305f71c887c857
RAMAN <raman.programmer87@gmail.com>
2024-05-18 14:54:44 +0330

    add a value for favorite screen to find out the type of video(movie, tv show)

--- RollMovie/app/src/main/java/com/raman/RollMovie/model/data/FavoriteModel.kt
+++ RollMovie/app/src/main/java/com/raman/RollMovie/model/data/FavoriteModel.kt
@@ -6,11 +6,12 @@ import androidx.room.PrimaryKey
 @Entity("favorite_table")
 data class FavoriteModel(
     @PrimaryKey
-    val id :Int,
-    val title :String,
-    val overview :String,
-    val imageUrl :String,
-    val vote :Double,
-    val realizeDate :String,
-    val adult :Boolean
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val imageUrl: String,
+    val vote: Double,
+    val realizeDate: String,
+    val adult: Boolean,
+    val isMovie: Boolean
 )
