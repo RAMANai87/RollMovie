838c90b38dcf2a1af0cea3f5513123a6fe88eafc
RAMAN <raman.programmer87@gmail.com>
2024-06-05 00:16:56 +0330

    remove profile screen

--- RollMovie/app/src/main/java/com/raman/RollMovie/ui/features/profile/EditProfile.kt
+++
@@ -1,6 +0,0 @@
-package com.raman.RollMovie.ui.features.profile
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun EditProfileScreen() {}
--- RollMovie/app/src/main/java/com/raman/RollMovie/ui/features/profile/Profile.kt
+++
@@ -1,8 +0,0 @@
-package com.raman.RollMovie.ui.features.profile
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun ProfileScreen() {
-
-}
