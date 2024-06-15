141a19131b2b94041c646d6caca51cf83077f39c
RAMAN <raman.programmer87@gmail.com>
2024-06-16 01:18:56 +0330

    i want to change it

--- RollMovie/app/build.gradle.kts
+++ RollMovie/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
