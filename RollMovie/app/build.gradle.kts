735f80cb3241bc2eb05078bb57179cdfc9fbffa9
RAMAN <raman.programmer87@gmail.com>
2024-06-21 17:45:39 +0330

    version 2 without signup or sign in user

--- RollMovie/app/build.gradle.kts
+++ RollMovie/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.raman.RollMovie"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
